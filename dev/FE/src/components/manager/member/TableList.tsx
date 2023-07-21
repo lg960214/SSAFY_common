a674c21918c3e84bab8751a679901e1dfe6dc350
seungwookim <osabero@naver.com>
2023-07-21 15:14:55 +0900

    ✨ _태그현황 내용_(#45)

---
+++ dev/FE/src/components/manager/member/TableList.tsx
@@ -0,0 +1,26 @@
+import { TagItem } from './TagItem';
+import dummy from './dummy.json';
+
+export const TableList = () => {
+  const data = dummy.data;
+  return (
+    <div className="bg-black w-96 h-96 text-white p-2 rounded-2xl">
+      <div className="flex justify-evenly items-center h-12 basis-32 text-center">
+        <span className="basis-1/3">이름</span>
+        <span className="basis-1/3">회원 번호</span>
+        <span className="basis-1/3">태그 번호</span>
+      </div>
+      <div className="border-white border-b-2 h-0"></div>
+      <div>
+        {data.map((item) => {
+          const tagItemProps = {
+            name: item.name,
+            memberNumber: item.userid,
+            tagNumber: item.tag,
+          };
+          return <TagItem key={item.userid} {...tagItemProps} />;
+        })}
+      </div>
+    </div>
+  );
+};
---
+++ dev/FE/src/components/manager/member/TagItem.tsx
@@ -0,0 +1,15 @@
+interface TagItemProps {
+  name: string;
+  memberNumber: string;
+  tagNumber: string | null;
+}
+
+export const TagItem = (tagItemProps: TagItemProps) => {
+  return (
+    <div className="flex justify-evenly items-center h-10 text-center">
+      <span className="basis-1/3">{tagItemProps.name}</span>
+      <span className="basis-1/3">{tagItemProps.memberNumber}</span>
+      <span className="basis-1/3">{tagItemProps.tagNumber}</span>
+    </div>
+  );
+};
