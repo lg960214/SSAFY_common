ef4af78ec0fe931536e113725eb4d87cdf9527ea
이승준 <ja132824@naver.com>
2023-07-24 16:15:48 +0900

    :sparkles:_이용현황 페이지 날짜변경/조회페이지네이션/그래프 구현_(#118)

--- dev/FE/src/App.tsx
+++ dev/FE/src/App.tsx
@@ -4,6 +4,7 @@ import Main from './pages/Main';
 import Tmp from './pages/Tmp';
 import Contact from './pages/Contact';
 import NavBar from './components/common/NavBar';
+import UsagePage from './pages/manager/UsagePage';
 import AuthProvider from './components/common/AuthProvider';
 import AuthContext from './components/common/AuthContext';
 
@@ -18,7 +19,7 @@ const App: React.FC = () => {
             <>
               <Route path="/member" element={<Main />} />
               <Route path="/equipment" element={<Tmp />} />
-              <Route path="/usage" element={<Contact />} />
+              <Route path="/usage" element={<UsagePage />} />
               <Route path="/waitlist" element={<Contact />} />
             </>
           ) : (
