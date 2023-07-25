b451655ca62d2cc6a2df72ccb567091303ea7f01
seungwookim <osabero@naver.com>
2023-07-25 10:52:18 +0900

    🎨 _Modal코드 수정_(#45)

--- dev/FE/src/components/common/Modal.tsx
+++ dev/FE/src/components/common/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode, useState, useEffect } from 'react';
+import React, { FC, ReactNode } from 'react';
 
 interface ModalProps {
   isOpen: boolean;
@@ -7,12 +7,6 @@ interface ModalProps {
 }
 
 const Modal: FC<ModalProps> = ({ isOpen, onClose, children }) => {
-  const [showModal, setShowModal] = useState(isOpen);
-
-  useEffect(() => {
-    setShowModal(isOpen);
-  }, [isOpen]);
-
   const handleBackgroundClick = (e: React.MouseEvent) => {
     onClose(); // 배경 클릭시 모달 닫기
     e.stopPropagation();
@@ -24,7 +18,7 @@ const Modal: FC<ModalProps> = ({ isOpen, onClose, children }) => {
 
   return (
     <>
-      {showModal && (
+      {isOpen && (
         <div
           className="modal-content fixed border-2 top-0 left-0 flex align-middle justify-center bg-white border-black w-full h-full z-20"
           onClick={handleBackgroundClick}
