179f05005253a8107deb504cf11a6acfdedf1c6d
ja132824 <ja132824@naver.com>
2023-08-03 06:18:41 +0000

    :art:_기로그인시 회원관리페이지로 바로 이동 기능추가_(#243)

--- dev/FE/src/pages/manager/MainPage.tsx
+++ dev/FE/src/pages/manager/MainPage.tsx
@@ -1,8 +1,18 @@
+import AuthContext from '@/components/common/AuthContext';
 import ContactSection from '@/components/manager/main/ContactSection';
 import ServiceSection from '@/components/manager/main/ServiceSection';
 import TitleSection from '@/components/manager/main/TitleSection';
-
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 const MainPage = () => {
+  const { isLoggedIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate('/member');
+    }
+  }, []);
+
   return (
     <>
       <div>
