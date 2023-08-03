e9d158db04648aa3c06e231cd0f6b3ad398d610a
ja132824 <ja132824@naver.com>
2023-08-03 05:57:48 +0000

    :art:_로그인 API 파일 일부 수정_(#243)

--- dev/FE/src/api/managerLoginApi.ts
+++ dev/FE/src/api/managerLoginApi.ts
@@ -3,17 +3,10 @@ import { LoginData } from '@/types/account.type';
 import axios from 'axios';
 
 const managerLoginApi = async (loginData: LoginData) => {
-  try {
-    const url = BASEURL + 'admin/login';
-    const response = await axios.post(url, loginData);
-
-    if (response.data.data === 'FAIL') throw Error('Login Failed');
-
-    return response.data;
-  } catch (error) {
-    alert('아이디 또는 비밀번호를 확인하세요!');
-    console.log(error);
-  }
+  const url = BASEURL + 'admin/login';
+  const response = await axios.post(url, loginData);
+  if (response.data.data === 'FAIL') throw Error('Login Failed');
+  return response.data;
 };
 
 export default managerLoginApi;
