613c303a371f54fe88685820a393f341d5151001
chchih99 <chchih99@gmail.com>
2023-08-08 07:11:12 +0000

    🎨 _대기현황 선택 시 선택 가능한 구역만 렌더링되도록 수정_(#313)

--- dev/FE/src/pages/manager/WaitListPage.tsx
+++ dev/FE/src/pages/manager/WaitListPage.tsx
@@ -1,14 +1,35 @@
 // WaitListPage.tsx
 import ViewSectionButton from '@/components/manager/waitlist/ViewSectionButton';
 import './waitlistpage.css';
+import { useQuery } from '@tanstack/react-query';
+import { Reader } from '@/types/reader.type';
+import { getReaders } from '@/api/equipmentApi';
+import { useEffect, useState } from 'react';
 const WaitListPage = () => {
-  const sectionData = ['A', 'B', 'C', 'D', 'E'];
-  const handleOpenNewTab = (url: string) => {
-    window.open(
-      `http://localhost:3000/waitlist/${url}`,
-      '_blank',
-      'width=1920,height=1080',
-    );
+  const [sectionList, setSectionList] = useState<(string | null)[]>(['A']);
+
+  const { data, isLoading } = useQuery<Reader[]>(['regions'], getReaders);
+
+  useEffect(() => {
+    if (data) {
+      const extractRegions = data
+        ?.map((cur) => cur.region)
+        .filter((cur) => !!cur);
+      const regionsSet = new Set(extractRegions);
+      const makeArray = [...regionsSet];
+      makeArray.sort();
+      setSectionList(makeArray);
+    }
+  }, [data, isLoading]);
+
+  const handleOpenNewTab = (url: string | null) => {
+    if (url) {
+      window.open(
+        `http://localhost:3000/waitlist/${url}`,
+        '_blank',
+        'width=1920,height=1080',
+      );
+    }
   };
 
   return (
@@ -18,13 +39,15 @@ const WaitListPage = () => {
       </div>
       <div>
         <ul className="flex selectbox mx-auto mt-20">
-          {sectionData.map((sect) => (
-            <ViewSectionButton
-              key={sect}
-              section={sect}
-              onClick={() => handleOpenNewTab(sect)}
-            />
-          ))}
+          {sectionList.map((sect) =>
+            sect ? (
+              <ViewSectionButton
+                key={sect}
+                section={sect}
+                onClick={() => handleOpenNewTab(sect)}
+              />
+            ) : null,
+          )}
         </ul>
       </div>
     </div>
