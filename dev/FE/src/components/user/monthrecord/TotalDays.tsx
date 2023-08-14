8be71de7c4e567535f217970459e6e1020929919
chihoonchoe <chchih99@gmail.com>
2023-08-14 15:40:26 +0900

    🐛 _Import Error Hotfix_(#0)

--- dev/FE/src/components/user/monthrecord/TotalDays.tsx
+++ dev/FE/src/components/user/monthrecord/TotalDays.tsx
@@ -1,5 +1,5 @@
 import { Exercise } from '@/pages/user/RecordPage';
-import SectionHeader from './sectionHeader';
+import SectionHeader from './SectionHeader';
 
 interface TotalDaysProps {
   exerciseList1: Exercise[];
