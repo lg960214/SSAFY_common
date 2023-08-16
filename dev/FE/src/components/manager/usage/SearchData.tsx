263b4a29011a1d422674285683d5111500350633
seungwookim <osabero@naver.com>
2023-08-16 16:10:31 +0900

    🎨 _pagination수정_(#369)

--- dev/FE/src/components/manager/usage/SearchData.tsx
+++ dev/FE/src/components/manager/usage/SearchData.tsx
@@ -1,6 +1,7 @@
 import { UsageData } from '@/types/usage.type';
 import { useState } from 'react';
 import ShowData from './ShowData';
+import ReactPaginate from 'react-paginate';
 
 interface SearchDataProps {
   dailyUsageData: UsageData[];
@@ -9,43 +10,38 @@ interface SearchDataProps {
 const SearchData = ({ dailyUsageData }: SearchDataProps) => {
   const itemsPerPage = 3; // 페이지당 표시할 항목의 개수
 
-  const [currentPage, setCurrentPage] = useState(1);
+  const [itemOffset, setItemOffset] = useState(0);
+  const endOffset = itemOffset + itemsPerPage;
+  const currentItems = dailyUsageData.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(dailyUsageData.length / itemsPerPage);
 
-  // 현재 페이지에 해당하는 데이터를 계산하는 함수
-  const getCurrentPageData = () => {
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    return dailyUsageData.slice(startIndex, endIndex);
+  const handlePageClick = (event: { selected: number }) => {
+    const newOffset = (event.selected * itemsPerPage) % dailyUsageData.length;
+    setItemOffset(newOffset);
   };
 
-  // 페이지 변경 시 호출되는 함수
-  const handlePageChange = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
-  };
-
-  const renderedData = getCurrentPageData();
   return (
     <>
-      <div className="float-left -mt-11 pt-5 w-[420px] h-[600px] rounded-[20px] bg-slate-200 mx-auto flex flex-col justify-between shadow-right-bottom shadow-gray-300">
+      <div className="float-left -mt-11 py-5 w-[420px] h-[600px] rounded-[20px] bg-slate-200 mx-auto flex flex-col justify-between shadow-right-bottom shadow-gray-300">
         <ul>
-          {renderedData.map((item) => (
+          {currentItems.map((item) => (
             <ShowData renderData={item} key={item.name} />
           ))}
         </ul>
-        <div className=" flex justify-center font-Bungee text-2xl ">
-          {Array.from(
-            { length: Math.ceil(dailyUsageData.length / itemsPerPage) },
-            (_, index) => (
-              <button
-                className="bg-slate-200 "
-                key={index + 1}
-                onClick={() => handlePageChange(index + 1)}
-              >
-                <span>{index + 1}</span>
-              </button>
-            ),
-          )}
-        </div>
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel=">"
+          nextLinkClassName="text-[#323554] hover:text-CustomOrange"
+          className="flex justify-center text-2xl font-Bungee"
+          pageLinkClassName="mx-2 hover:text-CustomOrange"
+          activeLinkClassName="font-extrabold text-CustomOrange"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="<"
+          previousLinkClassName="text-[#323554] hover:text-CustomOrange"
+          renderOnZeroPageCount={null}
+        />
       </div>
     </>
   );
